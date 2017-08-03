eb4a6f68142f8b22a9f80de7073cdac262aaddce
Braden Alsford <bal65@uclive.ac.nz>
2017-08-03 12:58:35 +1200

    Braden edits doc

--- mavenproj/src/main/java/seng202/team10/App.java
+++ mavenproj/src/main/java/seng202/team10/App.java
@@ -7,6 +7,7 @@ package seng202.team10;
 public class App 
 {
 //James is trying to edit this.
+//Braden is trying to edit this
     public static void main( String[] args )
     {
         System.out.println( "Hello World, this is in Git!" );
