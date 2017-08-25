7ef64dd758fc98ee4281bfa2c103316cbd975177
james <jto59@uclive.ac.nz>
2017-08-25 12:48:02 +1200

    updating MapkeyLocations

--- Main Build/src/main/java/mapElements/MapKeyLocations.java
+++ Main Build/src/main/java/mapElements/MapKeyLocations.java
@@ -1,10 +1,21 @@
 package mapElements;
 
-
+/**
+ * Holds data for key locations on the NY map
+  */
 
 public class MapKeyLocations {
     int longitude;
-    int latidute;
+    int latitute;
     String name;
     String city;
+
+    /**
+     * This method does something
+     */
+
+    public int getLongitude() {
+        return
+    }
+
 }
