b3f2858f9e0a875e002a5dab092d690c37505cf6
mki58 <mki58@uclive.ac.nz>
2017-08-26 12:27:50 +1200

    Added dataManipulation package and basic skeleton of DataFilterer class. Refactored mapElements package so it was recognised as a package by intelliJ.

---
+++ MainBuild/src/main/java/dataManipulation/DataFilterer.java
@@ -0,0 +1,23 @@
+package dataManipulation;
+
+import java.util.ArrayList;
+
+import mapElements.Route;
+
+
+/**
+ * DataFilterer class uses methods that sent requests to the database and return a array of data.
+ */
+public class DataFilterer {
+
+    private String databaseCommand;
+
+
+    public static ArrayList<Route> filterByGender() { return ArrayList; }
+
+    public static ArrayList<Route> filterByDate() { return ArrayList; }
+
+    public static ArrayList<Route> filterByAge() { return ArrayList; }
+
+    public static ArrayList<Route> filterByTime() { return ArrayList; }
+}
