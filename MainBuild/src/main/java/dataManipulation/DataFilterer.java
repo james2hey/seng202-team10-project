489a9f45373f8fb2c0589bbfc800c771325f59fe
Matt <mki58@uclive.ac.nz>
2017-08-28 15:56:07 +1200

    Refactoring DataFilterer class.

--- MainBuild/src/main/java/dataManipulation/DataFilterer.java
+++ MainBuild/src/main/java/dataManipulation/DataFilterer.java
@@ -15,21 +15,45 @@ public class DataFilterer {
 
     private String databaseCommand;
     private ArrayList<Route> routes = new ArrayList<>();
+    private int genderInteger =  -1;
 
 
     public void generateRouteArray(ResultSet rs) {
-        while (rs.next()) {
-            routes.add(new Route(rs.getInt("trip_duration"), rs.getInt("start_time"),
-                rs.getString("stop_time"), rs.getString("start_date"),
-                rs.getString("stop_date"), rs.getString("start_station_name"),
-                rs.getString("end_station_name"), rs.getString("start_station_latitude"),
-                rs.getString("start_station_longitude"),
-                rs.getString("end_station_latitude"),
-                rs.getString("end_station_longitude"), rs.getString("gender"),
-                rs.getString("date_of_birth"), rs.getString("start_station_id"),
-                rs.getString("end_station_id")));
+        try {
+            while (rs.next()) {
+                routes.add(new Route(rs.getInt("trip_duration"), rs.getInt("start_time"),
+                        rs.getString("stop_time"), rs.getString("start_date"),
+                        rs.getString("stop_date"), rs.getString("start_station_name"),
+                        rs.getString("end_station_name"), rs.getString("start_station_latitude"),
+                        rs.getString("start_station_longitude"),
+                        rs.getString("end_station_latitude"),
+                        rs.getString("end_station_longitude"), rs.getString("gender"),
+                        rs.getString("date_of_birth"), rs.getString("start_station_id"),
+                        rs.getString("end_station_id")));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
+
+
+    public ArrayList<Route> filterer() {
+        try(Connection conn = this.connect();
+            PreparedStatement pstmt = conn.prepareStatement(databaseCommand)) {
+
+            //set value using parameter
+            pstmt.setInt(1, genderInteger);
+
+            ResultSet rs = pstmt.executeQuery();
+            generateRouteArray(rs);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return routes;
+    }
+
+
     /**
      * filterByGender gets all route records that have the corresponding gender to the method parameter.
      *
@@ -38,7 +62,6 @@ public class DataFilterer {
      */
     public ArrayList<Route> filterByGender(String gender) {
 
-        int genderInteger = -1;
         if (gender == "F") {
             genderInteger = 2;
         } else if (gender == "M") {
@@ -50,25 +73,15 @@ public class DataFilterer {
                 "end_station_latitude, gender, date_of_birth, start_station_id, end_station_id FROM route_data WHERE " +
                 "gender = ?";
 
-        try(Connection conn = this.connect();
-            PreparedStatement pstmt = conn.prepareStatement(databaseCommand)) {
 
-            //set value using parameter
-            pstmt.setInt(1, genderInteger);
-
-            ResultSet rs = pstmt.executeQuery();
-            generateRouteArray(rs);
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return routes;
     }
+
 
     public static ArrayList<Route> filterByDate(String date) {
         ArrayList<Route> filteredData;
         return filteredData;
     }
+
 
     public ArrayList<Route> filterByAge(int age) {
 
@@ -78,20 +91,8 @@ public class DataFilterer {
                 "end_station_latitude, gender, date_of_birth, start_station_id, end_station_id FROM route_data WHERE " +
                 "date_of_birth = ?";
 
-        try(Connection conn = this.connect();
-            PreparedStatement pstmt = conn.prepareStatement(databaseCommand)) {
-
-            //set value using parameter
-            pstmt.setInt(1, genderInteger);
-
-            ResultSet rs = pstmt.executeQuery();
-            generateRouteArray(rs);
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return routes;
     }
+
 
     public static ArrayList<Route> filterByTime(String time) {
         ArrayList<Route> filteredData;
