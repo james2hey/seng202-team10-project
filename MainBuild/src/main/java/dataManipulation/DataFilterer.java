7073a1363894c61e9809f4addd89c36f68a8439b
Matt <mki58@uclive.ac.nz>
2017-08-29 12:49:18 +1200

    Adding methods to DataFilterer class. Updated DataFilterTest.

--- MainBuild/src/main/java/dataManipulation/DataFilterer.java
+++ MainBuild/src/main/java/dataManipulation/DataFilterer.java
@@ -2,10 +2,11 @@ package dataManipulation;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.time.Year;
 ////////////////////////////////
 
-import mapElements.Route;
-import mapElements.MapKeyLocations;
+import dataAnalysis.Route;
+import dataAnalysis.MapKeyLocations;
 
 
 /**
@@ -14,11 +15,42 @@ import mapElements.MapKeyLocations;
 public class DataFilterer {
 
     private String databaseCommand;
-    private ArrayList<Route> routes = new ArrayList<>();
-    private int genderInteger =  -1;
+    private ArrayList<Route> routes;
 
 
-    public void generateRouteArray(ResultSet rs) {
+    /**
+     * Constructor for DataFilterer class.
+     */
+    public DataFilterer() {
+        databaseCommand = "";
+        routes = new ArrayList<>();
+    }
+
+
+    /**
+     * connect class establishes a connection to the database. Used by all methods that request a query from the
+     * database.
+     *
+     * @return Connection
+     */
+    private Connection connect() {
+
+        String url = "";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    /**
+     * GenerateRouteArray takes a result set and creates a Route from each result and adds them to a ArrayList.
+     *
+     * @param rs rs is a result set of data records from a query to the database.
+     */
+    private void generateRouteArray(ResultSet rs) {
         try {
             while (rs.next()) {
                 routes.add(new Route(rs.getInt("trip_duration"), rs.getInt("start_time"),
@@ -37,7 +69,27 @@ public class DataFilterer {
     }
 
 
-    public ArrayList<Route> filterer() {
+    /**
+     * filterByGender takes a string that specifies the gender to filter records by and gets all route records that have
+     * the corresponding gender to the method parameter.
+     *
+     * @param gender gender of type String. Specifies the gender to filter records by.
+     */
+    public void filterByGender(String gender) {
+
+        int genderInteger = 0;
+
+        if ("F".equals(gender)) {
+            genderInteger = 2;
+        } else if ("M".equals(gender)) {
+            genderInteger = 1;
+        }
+
+        databaseCommand = "SELECT trip_duration, start_time, stop_time, start_date, stop_date, start_station_name, " +
+                "end_station_name, start_station_latitude, start_station_longitude, end_station_latitude, " +
+                "end_station_latitude, gender, date_of_birth, start_station_id, end_station_id FROM route_data WHERE " +
+                "gender = ?";
+
         try(Connection conn = this.connect();
             PreparedStatement pstmt = conn.prepareStatement(databaseCommand)) {
 
@@ -50,52 +102,105 @@ public class DataFilterer {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-        return routes;
     }
 
 
     /**
-     * filterByGender gets all route records that have the corresponding gender to the method parameter.
+     * filterByDate take an upper date and lower date and query's the database to find all records that were started
+     * between those dates.
      *
-     * @param gender gender of data records to be filtered.
-     * @return ArrayList<Route>
+     * @param upperDate upperDate is of type Date from the sql package. It is the upper limit that a route was
+     *                  started on, specified by the user.
+     * @param lowerDate lowerDate is of type Date from the sql package. It is the lower limit that a route was
+     *                  started on, specified by the user.
      */
-    public ArrayList<Route> filterByGender(String gender) {
+    public void filterByDate(Date upperDate, Date lowerDate) {
 
-        if (gender == "F") {
-            genderInteger = 2;
-        } else if (gender == "M") {
-            genderInteger = 1;
+        databaseCommand = "SELECT trip_duration, start_time, stop_time, start_date, stop_date, start_station_name, " +
+                "end_station_name, start_station_latitude, start_station_longitude, end_station_latitude, " +
+                "end_station_latitude, gender, date_of_birth, start_station_id, end_station_id FROM route_data WHERE " +
+                "start_date BETWEEN ? AND ?";
+
+        try(Connection conn = this.connect();
+            PreparedStatement pstmt = conn.prepareStatement(databaseCommand)) {
+
+            //set value using parameter
+            pstmt.setDate(1, lowerDate);
+            pstmt.setDate(2, upperDate);
+
+            ResultSet rs = pstmt.executeQuery();
+            generateRouteArray(rs);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
+    }
+
+
+    /**
+     * filterByAge takes an upper age limit and lower age limit and query's the database to find all records that were
+     * completed between those two ages.
+     *
+     * @param upperAge upperAge is of type int. It is the upper age limit of the person that completed a route, that the
+     *                 user wants to filter by.
+     * @param lowerAge lowerAge is of type int. It is the lower age limit of the person that completed a route, that the
+     *                 user wants to filter by.
+     */
+    public void filterByAge(int upperAge, int lowerAge) {
+
+        int year = Year.now().getValue();
+        int LowerRequiredYear = year - upperAge;
+        int UpperRequiredYear = year - lowerAge;
 
         databaseCommand = "SELECT trip_duration, start_time, stop_time, start_date, stop_date, start_station_name, " +
                 "end_station_name, start_station_latitude, start_station_longitude, end_station_latitude, " +
                 "end_station_latitude, gender, date_of_birth, start_station_id, end_station_id FROM route_data WHERE " +
-                "gender = ?";
+                "date_of_birth BETWEEN ? AND ?";
 
+        try(Connection conn = this.connect();
+            PreparedStatement pstmt = conn.prepareStatement(databaseCommand)) {
 
+            //set value using parameter
+            pstmt.setInt(1, LowerRequiredYear);
+            pstmt.setInt(2, UpperRequiredYear);
+
+            ResultSet rs = pstmt.executeQuery();
+            generateRouteArray(rs);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 
-    public static ArrayList<Route> filterByDate(String date) {
-        ArrayList<Route> filteredData;
-        return filteredData;
-    }
-
-
-    public ArrayList<Route> filterByAge(int age) {
-
+    /**
+     * filterByTime takes an upper time limit and lower time limit and query's the database to find all records that
+     * were started between those times.
+     *
+     * @param upperTime upperTime is of type Time from the sql package. It is the upper time limit of starting a route
+     *                  the user wants to filter by.
+     * @param lowerTime lowerTime is of type Time from the sql package. It is the lower time limit of starting a route
+     *                  the user wants to filter by.
+     */
+    public void filterByTime(Time upperTime, Time lowerTime) {
 
         databaseCommand = "SELECT trip_duration, start_time, stop_time, start_date, stop_date, start_station_name, " +
                 "end_station_name, start_station_latitude, start_station_longitude, end_station_latitude, " +
                 "end_station_latitude, gender, date_of_birth, start_station_id, end_station_id FROM route_data WHERE " +
-                "date_of_birth = ?";
+                "start_time BETWEEN ? AND ?";
 
-    }
+        try(Connection conn = this.connect();
+            PreparedStatement pstmt = conn.prepareStatement(databaseCommand)) {
 
+            //set value using parameter
+            pstmt.setTime(1, lowerTime);
+            pstmt.setTime(2, upperTime);
 
-    public static ArrayList<Route> filterByTime(String time) {
-        ArrayList<Route> filteredData;
-        return filteredData;
+            ResultSet rs = pstmt.executeQuery();
+            generateRouteArray(rs);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
