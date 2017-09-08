8213331f823fe9abe2873baff3580c5a2ad2a63d
Braden Alsford <bal65@uclive.ac.nz>
2017-09-08 14:10:15 +1200

    Implemented basic GUI skeleton

--- MainBuild/src/main/java/main/Controller.java
+++ MainBuild/src/main/java/main/Controller.java
@@ -1,31 +1,102 @@
 package main;
 
-import com.sun.org.apache.xpath.internal.operations.Mod;
-import javafx.fxml.FXMLLoader;
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.stage.Modality;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
 public class Controller {
 
-    public void showLogin() {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-            Stage stage = new Stage();
+    @FXML
+    private Button button;
 
-            stage.initModality(Modality.APPLICATION_MODAL);
-            stage.setResizable(false);
-            stage.setTitle("Login Window");
-            stage.setScene(new Scene(root, 400, 250));
-            stage.show();
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setAlertType(Alert.AlertType.CONFIRMATION);
-        } catch (IOException e) {
-            e.printStackTrace();
+    @FXML
+    private JFXDrawer drawer;
+
+    @FXML
+    private Text buttonPressedText;
+
+    @FXML
+    private JFXHamburger hamburger;
+
+    @FXML
+    private Button addDataButton;
+
+    @FXML
+    private Button viewDataButton;
+
+    @FXML
+    private Button planRouteButton;
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    void login() {
+        System.exit(0);
+    }
+
+    @FXML
+    void openDrawer() throws IOException {
+        initializeSideDrawer();
+
+        if (drawer.isShown()) {
+            drawer.close();
+        }
+        else {
+            drawer.open();
         }
     }
+
+    @FXML
+    void changeToPlanRouteScene(ActionEvent event) throws IOException {
+        Parent planRouteParent = FXMLLoader.load(getClass().getClassLoader().getResource("planRoute.fxml"));
+        Scene planRouteScene = new Scene(planRouteParent);
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.setScene(planRouteScene);
+    }
+
+    @FXML
+    void changeToHomeScene(ActionEvent event) throws IOException {
+        Parent homeParent = FXMLLoader.load(getClass().getClassLoader().getResource("home.fxml"));
+        Scene homeScene = new Scene(homeParent);
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.setScene(homeScene);
+    }
+
+
+    @FXML
+    void changeToAddDataScene(ActionEvent event) throws IOException {
+        Parent addDataParent = FXMLLoader.load(getClass().getClassLoader().getResource("addData.fxml"));
+        Scene addDataScene = new Scene(addDataParent);
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.setScene(addDataScene);
+    }
+
+    @FXML
+    void changeToViewDataScene(ActionEvent event) throws IOException {
+        Parent viewDataParent = FXMLLoader.load(getClass().getClassLoader().getResource("viewData.fxml"));
+        Scene viewDataScene = new Scene(viewDataParent);
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.setScene(viewDataScene);
+    }
+
+
+    //@Override
+    public void initializeSideDrawer() throws IOException {
+        VBox box = FXMLLoader.load(getClass().getClassLoader().getResource("SidePanel.fxml"));
+        drawer.setSidePane(box);
+    }
+
 }
