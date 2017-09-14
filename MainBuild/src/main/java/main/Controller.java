4225b08caea9acd66dea07ce3eb7b9b740505304
Jack Steel <jes143@uclive.ac.nz>
2017-09-15 03:08:39 +1200

    Adding GmapsFX and getting some very rudamentary mapping working, it's commented out becuase it only works under certain conditions

--- MainBuild/src/main/java/main/Controller.java
+++ MainBuild/src/main/java/main/Controller.java
@@ -2,12 +2,24 @@ package main;
 
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.object.*;
+
+import com.lynden.gmapsfx.service.geocoding.GeocoderStatus;
+import com.lynden.gmapsfx.service.geocoding.GeocodingResult;
+import com.lynden.gmapsfx.service.geocoding.GeocodingService;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.fxml.FXMLLoader;
@@ -15,8 +27,10 @@ import javafx.stage.Stage;
 
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable, MapComponentInitializedListener {
 
     @FXML
     private Button button;
@@ -41,6 +55,22 @@ public class Controller {
 
     @FXML
     private Button homeButton;
+
+    @FXML
+    private TextField addressTextField;
+
+    @FXML
+    private GoogleMapView mapView;
+
+    private GeocodingService geocodingService;
+
+    private GoogleMap map;
+
+    private StringProperty address = new SimpleStringProperty();
+
+    private MarkerOptions point1opt;
+
+    private Marker point1;
 
     @FXML
     void login() {
@@ -104,4 +134,54 @@ public class Controller {
         drawer.setSidePane(box);
     }
 
+    @Override
+    public void mapInitialized() {
+        geocodingService = new GeocodingService();
+        MapOptions mapOptions = new MapOptions();
+
+        mapOptions.center(new LatLong(-43.5235375, 172.5839233))
+                .mapType(MapTypeIdEnum.ROADMAP)
+                .overviewMapControl(false)
+                .panControl(false)
+                .rotateControl(false)
+                .scaleControl(false)
+                .streetViewControl(false)
+                .zoomControl(false)
+                .zoom(12);
+
+        map = mapView.createMap(mapOptions);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        mapView.addMapInializedListener(this);
+        address.bind(addressTextField.textProperty());
+        point1opt = new MarkerOptions();
+    }
+
+    @FXML
+    public void addressTextFieldAction(ActionEvent event) {
+        geocodingService.geocode(address.get(), (GeocodingResult[] results, GeocoderStatus status) -> {
+
+            LatLong latLong = null;
+
+            if( status == GeocoderStatus.ZERO_RESULTS) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "No matching address found");
+                alert.show();
+                return;
+            } else if( results.length > 1 ) {
+                Alert alert = new Alert(Alert.AlertType.WARNING, "Multiple results found, showing the first one.");
+                alert.show();
+                latLong = new LatLong(results[0].getGeometry().getLocation().getLatitude(), results[0].getGeometry().getLocation().getLongitude());
+            } else {
+                latLong = new LatLong(results[0].getGeometry().getLocation().getLatitude(), results[0].getGeometry().getLocation().getLongitude());
+            }
+
+            map.setCenter(latLong);
+            point1opt.position(latLong);
+            point1 = new Marker(point1opt);
+            map.addMarker(point1);
+
+        });
+    }
 }
