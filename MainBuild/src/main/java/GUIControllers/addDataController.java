1865992d65e33498950c52556871a881fc052253
Braden Alsford <bal65@uclive.ac.nz>
2017-09-16 18:25:25 +1200

    Created empty classes for different scene controllers

---
+++ MainBuild/src/main/java/GUIControllers/addDataController.java
@@ -0,0 +1,7 @@
+package GUIControllers;
+
+/**
+ * Created by bal65 on 16/09/17.
+ */
+public class addDataController {
+}
--- MainBuild/src/main/java/main/Main.java
+++ MainBuild/src/main/java/main/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 1100, 650));
         primaryStage.show();
         initCyclist();
+        /*
         DatabaseManager.connect();
         //DatabaseManager.countRows();   //function used to determine key for new records --MUST RUN ON START--
         CSV_Importer.readcsv(getClass().getClassLoader().getResource("Lower_Manhattan_Retailers-test.csv").getFile(), 1);
@@ -24,6 +25,7 @@ public class Main extends Application {
         CSV_Importer.readcsv(getClass().getClassLoader().getResource("201601-citibike-tripdata-test.csv").getFile(), 3);
         DatabaseManager.commit();
         DatabaseManager.printTables();
+        */
     }
 
     /**Initializes the user and does
