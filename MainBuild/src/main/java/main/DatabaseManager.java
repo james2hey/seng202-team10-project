eb16c5fc2afd8e4d997ccaa35186b420ebaadeba
Jack Steel <jes143@uclive.ac.nz>
2017-09-16 18:05:47 +1200

    Changing DB schema to add seperate date columns for each DD/MM/YY

--- MainBuild/src/main/java/main/DatabaseManager.java
+++ MainBuild/src/main/java/main/DatabaseManager.java
@@ -11,11 +11,15 @@ import java.sql.*;
 public class DatabaseManager {
 
     static String sql_trips = "CREATE TABLE IF NOT EXISTS route_information(" +
-            "   tripduration             INTEGER" +
-            "  ,start_date               VARCHAR(19)" +
-            "  ,start_time               VARCHAR(19)" +
-            "  ,end_date                 VARCHAR(19)" +
-            "  ,end_time                 VARCHAR(19)" +
+            "   tripduration            INTEGER" +
+            "  ,start_year              INTEGER" +
+            "  ,start_month             INTEGER" +
+            "  ,start_day               INTEGER" +
+            "  ,start_time              VARCHAR(19)" +
+            "  ,end_year                INTEGER" +
+            "  ,end_month               INTEGER" +
+            "  ,end_day                 INTEGER" +
+            "  ,end_time                VARCHAR(19)" +
             "  ,start_station_id        INTEGER" +
             "  ,start_station_name      VARCHAR(21)" +
             "  ,start_latitude          NUMERIC(9,6) NOT NULL" +
@@ -28,7 +32,7 @@ public class DatabaseManager {
             "  ,usertype                VARCHAR(10)" +
             "  ,birth_year              INTEGER" +
             "  ,gender                  INTEGER" +
-            "  ,PRIMARY KEY(start_time, bikeid))";
+            "  ,PRIMARY KEY(start_year, start_month, start_day, start_time, bikeid))";
 
     static String sql_retailers = "CREATE TABLE IF NOT EXISTS retailer(" +
             "   RETAILER_NAME      VARCHAR(50) NOT NULL" +
@@ -58,7 +62,7 @@ public class DatabaseManager {
 
     static String addRetailerString = "insert into retailer values(?,?,?,?,?,?,?,?,?)";
     static String addWifiString = "insert into wifi_location values(?,?,?,?,?,?,?,?,?,?,?)";
-    static String addTripString = "insert into route_information values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    static String addTripString = "insert into route_information values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
     static Connection conn = null;
     static Statement stmt = null;
     static PreparedStatement addRetailer = null;
@@ -225,32 +229,36 @@ public class DatabaseManager {
         }
     }
 
-    public static void addTrip(int tripduration, String start_date, String start_time, String end_date, String end_time, String start_station_id,
+    public static void addTrip(int tripduration, int start_year, int start_month, int start_day, String start_time, int end_year, int end_month, int end_day, String end_time, String start_station_id,
                                String start_station_name, double start_latitude, double start_longitude,
                                String end_station_id, String end_station_name, double end_latitude,
                                double end_longitude, String bikeid, String usertype, int birth_year, int gender) {
         try {
             conn.setAutoCommit(false);
             addTrip.setInt(1, tripduration);
-            addTrip.setString(2, start_date);
-            addTrip.setString(3, start_time);
-            addTrip.setString(4, end_date);
-            addTrip.setString(5, end_time);
-            addTrip.setString(6, start_station_id);
-            addTrip.setString(7, start_station_name);
-            addTrip.setDouble(8,start_latitude);
-            addTrip.setDouble(9, start_longitude);
-            addTrip.setString(10, end_station_id);
-            addTrip.setString(11, end_station_name);
-            addTrip.setDouble(12, end_latitude);
-            addTrip.setDouble(13, end_longitude);
-            addTrip.setString(14, bikeid);
-            addTrip.setString(15, usertype);
-            addTrip.setInt(16, birth_year);
+            addTrip.setInt(2, start_year);
+            addTrip.setInt(3, start_month);
+            addTrip.setInt(4, start_day);
+            addTrip.setString(5, start_time);
+            addTrip.setInt(6, end_year);
+            addTrip.setInt(7, end_month);
+            addTrip.setInt(8, end_day);
+            addTrip.setString(9, end_time);
+            addTrip.setString(10, start_station_id);
+            addTrip.setString(11, start_station_name);
+            addTrip.setDouble(12,start_latitude);
+            addTrip.setDouble(13, start_longitude);
+            addTrip.setString(14, end_station_id);
+            addTrip.setString(15, end_station_name);
+            addTrip.setDouble(16, end_latitude);
+            addTrip.setDouble(17, end_longitude);
+            addTrip.setString(18, bikeid);
+            addTrip.setString(19, usertype);
+            addTrip.setInt(20, birth_year);
             if (birth_year == -1) {
-                addTrip.setNull(16, Types.INTEGER);
+                addTrip.setNull(20, Types.INTEGER);
             }
-            addTrip.setInt(17, gender);
+            addTrip.setInt(21, gender);
             addTrip.executeUpdate();
             edits --;
 
