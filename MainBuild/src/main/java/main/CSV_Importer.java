a2caf347aa2dbd5b94d1d3876b5592dd872c7e83
Jack Steel <jes143@uclive.ac.nz>
2017-09-16 18:11:45 +1200

    Updating DB Schema to switch from one date field to 3 seperate YY/MM/DD

--- MainBuild/src/main/java/main/CSV_Importer.java
+++ MainBuild/src/main/java/main/CSV_Importer.java
@@ -35,13 +35,17 @@ public class CSV_Importer {
                     double end_lat = Double.parseDouble(record[9]);
                     double end_lon = Double.parseDouble(record[10]);
                     String[] date_time_start = record[1].split(" ");
+                    String[] date_start = date_time_start[0].split("/");
+
                     String[] date_time_end = record[2].split(" ");
+                    String[] date_end = date_time_start[0].split("/");
+
                     int birth_year = -1;
                     if (! "".equals(record[13])) {
                         birth_year = Integer.parseInt(record[13]);
                     }
                     int gender = Integer.parseInt(record[14]);
-                    DatabaseManager.addTrip(duration, date_time_start[0], date_time_start[1], date_time_end[0], date_time_end[1], record[3], record[4], start_lat, start_lon, record[7], record[8], end_lat, end_lon, record[11], record[12], birth_year, gender);
+                    DatabaseManager.addTrip(duration, Integer.parseInt(date_start[2]), Integer.parseInt(date_start[0]), Integer.parseInt(date_start[1]), date_time_start[1], Integer.parseInt(date_end[2]), Integer.parseInt(date_end[0]), Integer.parseInt(date_end[1]), date_time_end[1], record[3], record[4], start_lat, start_lon, record[7], record[8], end_lat, end_lon, record[11], record[12], birth_year, gender);
                 }
             }
         } catch (IOException e) {
