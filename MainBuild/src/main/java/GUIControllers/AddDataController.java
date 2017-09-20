dadbe0556e7f01aa082d7db0e04a043b30782767
Lewis W <lwh34@uclive.ac.nz>
2017-09-20 17:08:16 +1200

    Added type check to most things and most code for the single line now need to link them!

--- MainBuild/src/main/java/GUIControllers/AddDataController.java
+++ MainBuild/src/main/java/GUIControllers/AddDataController.java
@@ -20,6 +20,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import main.HandleUsers;
 import main.Main;
 
 
@@ -40,23 +41,28 @@ public class AddDataController extends Controller implements Initializable {
     @FXML
     private Button routeButton, wifiButton, retailerButton;
 
-    @FXML
-    private SplitPane wifiPanel, retailerPanel, endPanel, startPanel;
-
 
     @FXML //This importButton reveals other buttons
     private Button importButton, importRoute, importRetailer, importWifi, addDataButton;
 
-    @FXML
-    private Text inputError;
+    @FXML // Route Errors
+    private Text sLongError, sLatError, sTimeError, sDateError, eLongError, eLatError, eTimeError, eDateError;
 
-    @FXML
+    @FXML //Route Fields
     private JFXTextField rSAddress, rEAddress, rSLongitude, rELongitude, rSLatitude, rELatitude, rSTime, rETime, rSDate, rEDate;
+
+    @FXML // Retailer Fields
+    private JFXTextField retailerName, retailerAddress, retailerLong, retailerLat, retailerPrim, retailerSec;
+
+    @FXML // Wifi Fields
+    private JFXTextField wifiName, wifiLong, wifiLat, wifiAddress, wifiPostcode, wifiComments;
+
+    @FXML // Wifi & Retailer Errors
+    private Text wifiLatError, wifiLongError, retailerLatError, retailerLongError;
 
     @FXML
     private JFXDrawer drawer;
 
-    private int singleLineType = 0;
     private SQLiteDB db;
     private RetailerDataHandler retailerDataHandler;
     private WifiDataHandler wifiDataHandler;
@@ -68,117 +74,165 @@ public class AddDataController extends Controller implements Initializable {
         drawer.setSidePane(box);
         if (drawer.isShown()) {
             drawer.close();
-        }
-        else {
+        } else {
             drawer.open();
         }
     }
 
-    //TODO: Link with Matt for singular entries. Also make them invisible to start without losing children.
-    @FXML //Relates to the manual data page
-    void routeEntry(ActionEvent event) throws IOException {
-        if(!startPanel.isVisible()) {
-            startPanel.setVisible(true);
-        }
-        if(!endPanel.isVisible()) {
-            endPanel.setVisible(true);
-        }
-        if(wifiPanel.isVisible()) {
-            wifiPanel.setVisible(false);
-        }
-        if(retailerPanel.isVisible()) {
-            retailerPanel.setVisible(false);
-        }
-        singleLineType = 1;
-    }
-
-    @FXML //Relates to the manual data page
-    void wifiEntry(ActionEvent event) throws IOException {
-        if(startPanel.isVisible()) {
-            startPanel.setVisible(false);
-        }
-        if(endPanel.isVisible()) {
-            endPanel.setVisible(false);
-        }
-        if(!wifiPanel.isVisible()) {
-            wifiPanel.setVisible(true);
-        }
-        if(retailerPanel.isVisible()) {
-            retailerPanel.setVisible(false);
-        }
-        singleLineType = 2;
-    }
-    @FXML //Relates to the manual data page
-    void retailerEntry(ActionEvent event) throws IOException {
-        if(startPanel.isVisible()) {
-            startPanel.setVisible(false);
-        }
-        if(endPanel.isVisible()) {
-            endPanel.setVisible(false);
-        }
-        if(wifiPanel.isVisible()) {
-            wifiPanel.setVisible(false);
-        }
-        if(!retailerPanel.isVisible()) {
-            retailerPanel.setVisible(true);
-        }
-        singleLineType = 3;
-    }
-
     //Converts Dates to the right format for CSV insertion.
-    private int[] convertDates(String dateLower, String dateUpper) {
-        int dateInts[] = new int[6];
-        if(dateLower == null || dateUpper == null || dateLower.length() != 10 || dateUpper.length() != 10) {
-
-            System.out.print(dateLower);
+    private int[] convertDates(String date) {
+        int dateInts[] = new int[3];
+        if (date == null || date.length() != 10) {
             return null;
         }
-        dateInts[0] = Integer.parseInt(dateLower.substring(0, 2));
-        dateInts[1] = Integer.parseInt(dateLower.substring(3, 5));
-        dateInts[2] = Integer.parseInt(dateLower.substring(6));
-        dateInts[3] = Integer.parseInt(dateUpper.substring(0, 2));
-        dateInts[4] = Integer.parseInt(dateUpper.substring(3, 5));
-        dateInts[5] = Integer.parseInt(dateUpper.substring(6));
+        dateInts[0] = Integer.parseInt(date.substring(0, 2));
+        dateInts[1] = Integer.parseInt(date.substring(3, 5));
+        dateInts[2] = Integer.parseInt(date.substring(6));
+
         return dateInts;
     }
 
 
-    @FXML //TODO: Add more error types? idfk
-    void singleCSVLine(ActionEvent event) throws IOException {
-        if(singleLineType == 0) {
-            System.out.print("We are here");
+    @FXML
+        //TODO: Add more error types? idfk
+    void routeCSVLine(ActionEvent event) throws IOException {
+        double SLatitude = 0, SLongitude = 0, ELatitude = 0, ELongitude = 0;
+        boolean errorOccured = false;
+        int[] sDate = new int[3];
+        int[] eDate = new int[3];
+        String username = HandleUsers.currentCyclist.getName();
+
+        try { //Start Date
+            sDateError.setVisible(false);
+            sDate = convertDates(rSDate.getText());
+            if(sDate == null){
+                errorOccured = true;
+                sDateError.setVisible(true);
+            }
+        }
+        catch(Exception e) {
+            sDateError.setVisible(true);
+            errorOccured = true;
+        }
+        try { // Start Latitude
+            sLatError.setVisible(false);
+            SLatitude = Double.parseDouble(rSLatitude.getText());
+        } catch (Error e) {
+            sLatError.setVisible(true);
+            errorOccured = true;
+
+        }
+        try { // Start Longitude
+            sLongError.setVisible(false);
+            SLongitude = Double.parseDouble(rSLongitude.getText());
+        } catch (Exception e) {
+            errorOccured = true;
+            sLongError.setVisible(true);
+
+        }
+
+        try { // End Date
+            //eDateError.setVisible(false);
+            eDate = convertDates(rSDate.getText());
+            if(eDate == null){
+                errorOccured = true;
+                //eDateError.setVisible(true);
+            }
+        } catch(Exception e){
+            errorOccured = true;
+        }
+
+        try { //End Latitude
+            eLatError.setVisible(false);
+            ELatitude = Double.parseDouble(rELatitude.getText());
+        } catch (Exception e) {
+            errorOccured = true;
+            eLatError.setVisible(true);
+        }
+
+
+        try {// End Longitude
+            eLongError.setVisible(false);
+            ELongitude = Double.parseDouble(rELongitude.getText());
+        } catch (Exception e) {
+            eLongError.setVisible(true);
+            errorOccured = true;
+        }
+
+
+
+
+        if(errorOccured == true){
             return;
         }
 
-        if(singleLineType == 1){
-            double SLatitude, SLongitude, ELatitude, ELongitude;
+        /*RouteDataHandler newRoute = new RouteDataHandler();
+        Boolean fromHandler = newRoute.addSingleEntry(0, sDate[2], sDate[1], sDate[0], rSTime.getText(),
+                eDate[2], eDate[1], eDate[0], rETime.getText(), "1",
+                "Start", SLatitude, SLongitude, "2", "End", ELatitude, ELongitude,
+                "1", username, 2017, 1);*/
+
+    }
 
 
-            int[] dateInts = convertDates(rSDate.getText(), rEDate.getText());
-            System.out.println(rSDate.getText() + "accessible date");
-            if(dateInts == null) {
-                System.out.print("here");
-                inputError.setVisible(true);
-                return;
-            }
-            try {
-                SLatitude = Double.parseDouble(rSLatitude.getText());
-                SLongitude = Double.parseDouble(rSLongitude.getText());
-                ELatitude = Double.parseDouble(rELatitude.getText());
-                ELongitude = Double.parseDouble(rELongitude.getText());
+    @FXML
+        //TODO: Add more error types? idfk
+    void retailerCSVLine(ActionEvent event) throws IOException {
+        Boolean errorOccured = false;
+        Double retLat = 0.0, retLong = 0.0;
 
-
-            } catch(Error e) {
-                inputError.setVisible(true);
-                return;
-            }
-
-            routeDataHandler.addSingleEntry(0, dateInts[2], dateInts[1], dateInts[0], rSTime.getText(),
-                    dateInts[6], dateInts[5], dateInts[4], rETime.getText(), "1",
-                    "Start", SLatitude, SLongitude, "2", "End", ELatitude, ELongitude,
-                    "1", "User", 2017, 1);
+        try {
+            retailerLongError.setVisible(false);
+            retLong = Double.parseDouble(retailerLong.getText());
+        } catch (Exception e) {
+            retailerLongError.setVisible(true);
+            errorOccured = true;
         }
-        inputError.setVisible(false);
+
+        try {
+            retailerLatError.setVisible(false);
+            retLat = Double.parseDouble(retailerLat.getText());
+        } catch (Exception e) {
+            errorOccured = true;
+            retailerLatError.setVisible(true);
+        }
+
+        if(errorOccured == true){
+            return;
+        }
+        /*RetailerDataHandler newRetailer = new RetailerDataHandler();
+        Boolean fromHandler = newRetailer.addSingleEntry(retailerName.getText(), retailerAddress.getText(), retLat, retLong, null,
+                null, null, retailerPrim.getText(), retailerSec.getText());*/
+    }
+
+    @FXML
+        //TODO: Add more error types? idfk
+    void wifiCSVLine(ActionEvent event) throws IOException {
+        Boolean errorOccured = false;
+        Double wLat = 0.0, wLong = 0.0;
+
+        try {
+            wifiLongError.setVisible(false);
+            wLat = Double.parseDouble(wifiLong.getText());
+        } catch (Error e) {
+            wifiLongError.setVisible(true);
+            errorOccured = true;
+        }
+
+        try {
+            wifiLatError.setVisible(false);
+            wLong = Double.parseDouble(wifiLat.getText());
+        } catch (Exception e) {
+            errorOccured = true;
+            wifiLatError.setVisible(true);
+        }
+
+        if(errorOccured == true){
+            return;
+        }
+        /*WifiDataHandler newWifi = new RetailerDataHandler();
+        Boolean fromHandler = newWifi.addSingleEntry(wifiName.getText(), "", "", wifiAddress.getText(), wLat, wLong,
+        wifiComments.getText(), "", "", "", wifiPostcode.getText());*/
     }
 
 
@@ -191,6 +245,10 @@ public class AddDataController extends Controller implements Initializable {
             importWifi.setVisible(true);
         }
     }
+
+
+
+
     @FXML // Import file -> only allows *.csv and prints location afterwards for now...
         //TODO: Link with Matt to add to database. Fix efficiency of three blocks (try make it 1 block/method).
     void chooseRoute(ActionEvent event) throws IOException {
