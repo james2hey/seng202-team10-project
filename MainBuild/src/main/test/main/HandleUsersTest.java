d9dff1546666e69a81096c03cc7f0cca40c9f0d0
jto59 <jto59@uclive.ac.nz>
2017-09-25 17:46:10 +1300

    Pushing favourite test files

--- MainBuild/src/main/test/main/HandleUsersTest.java
+++ MainBuild/src/main/test/main/HandleUsersTest.java
@@ -3,7 +3,7 @@ package main;
 import dataAnalysis.RetailLocation;
 import dataAnalysis.Route;
 import dataAnalysis.WifiLocation;
-import dataHandler.SQLiteDB;
+import dataHandler.*;
 import dataManipulation.DataFilterer;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -20,18 +20,20 @@ import static org.junit.Assert.assertEquals;
  * Created by jto59 on 24/09/17.
  */
 public class HandleUsersTest {
-    private DataFilterer dataFilterer;
-    private ArrayList<Route> routes = new ArrayList<>();
-    private ArrayList<WifiLocation> wifiLocations = new ArrayList<>();
-    private ArrayList<RetailLocation> retailLocations = new ArrayList<>();
+//    private DataFilterer dataFilterer;
+//    private ArrayList<Route> routes = new ArrayList<>();
+//    private ArrayList<WifiLocation> wifiLocations = new ArrayList<>();
+//    private ArrayList<RetailLocation> retailLocations = new ArrayList<>();
 
-    private ArrayList<String> userList = new ArrayList<>();
-    public Cyclist currentCyclist;
+//    private ArrayList<String> userList = new ArrayList<>();
+//    public Cyclist currentCyclist;
 
     private SQLiteDB db;
+    private Cyclist currentCyclist;
+    private HandleUsers hu;
 
     @AfterClass
-    public static void tearDownClass() throws Exception {
+    public static void clearDB() throws Exception {
         String home = System.getProperty("user.home");
         java.nio.file.Path path = java.nio.file.Paths.get(home, "testdatabase.db");
         Files.delete(path);
@@ -42,18 +44,23 @@ public class HandleUsersTest {
         String home = System.getProperty("user.home");
         java.nio.file.Path path = java.nio.file.Paths.get(home, "testdatabase.db");
         db = new SQLiteDB(path.toString());
-        dataFilterer = new DataFilterer(db);
 
-//        WifiDataHandler wdh = new WifiDataHandler(db);
-//        wdh.processCSV(getClass().getClassLoader().getResource("CSV/NYC_Free_Public_WiFi_03292017-test.csv").getFile());
-//
-//        RouteDataHandler rdh = new RouteDataHandler(db);
-//        rdh.processCSV(getClass().getClassLoader().getResource("CSV/201601-citibike-tripdata-test.csv").getFile());
-//
-//        RetailerDataHandler retailerDataHandler = new RetailerDataHandler(db);
-//        System.out.println("Made");
-//        retailerDataHandler.processCSV(getClass().getClassLoader().getResource("CSV/Lower_Manhattan_Retailers-test.csv").getFile());
-//
+        currentCyclist = new Cyclist("Tester");
+        hu = new HandleUsers();
+
+        WifiDataHandler wdh = new WifiDataHandler(db);
+        System.out.println(wdh);
+        wdh.processCSV(getClass().getClassLoader().getResource("CSV/NYC_Free_Public_WiFi_03292017-test.csv").getFile());
+
+        RouteDataHandler rdh = new RouteDataHandler(db);
+        rdh.processCSV(getClass().getClassLoader().getResource("CSV/201601-citibike-tripdata-test.csv").getFile());
+
+        RetailerDataHandler retailerDataHandler = new RetailerDataHandler(db);
+        retailerDataHandler.processCSV(getClass().getClassLoader().getResource("CSV/Lower_Manhattan_Retailers-test.csv").getFile());
+
+        FavouriteRetailData favouriteRetailData = new FavouriteRetailData(db);
+
+
 
     }
 
@@ -93,8 +100,7 @@ public class HandleUsersTest {
 
     @Test
     public void logOutOfUser() throws Exception {
-        currentCyclist = new Cyclist("Tester");
-        HandleUsers hu = new HandleUsers();
+
         hu.logOutOfUser();
         assertEquals(null, hu.currentCyclist);
     }
