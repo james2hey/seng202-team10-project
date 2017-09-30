97a70a7a5aec183cb8ad367c279c3e7281087d15
Lewis W <lwh34@uclive.ac.nz>
2017-09-30 19:03:52 +1300

    Added bicycle icon

--- MainBuild/src/main/java/main/Main.java
+++ MainBuild/src/main/java/main/Main.java
@@ -7,7 +7,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
 
 public class Main extends Application {
     static SQLiteDB db;
@@ -28,8 +32,10 @@ public class Main extends Application {
         FavouriteWifiData favWifiData = new FavouriteWifiData(db);
         FavouriteRetailData favRetailData = new FavouriteRetailData(db);
 
-
+        String url = getClass().getClassLoader().getResource("Images/bicycleIcon.png").toString();
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("FXML/startUp.fxml"));
+
+        primaryStage.getIcons().add(new Image(url));
 
         primaryStage.setTitle("Pedals");
         primaryStage.setScene(new Scene(root, 1100, 650));
