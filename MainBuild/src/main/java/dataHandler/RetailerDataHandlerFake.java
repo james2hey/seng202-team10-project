25d51c623b34ea2993057bcf76017a6f740aed4a
Jack Steel <jes143@uclive.ac.nz>
2017-10-06 21:14:01 +1300

    Updated RetailerDataHanderFake to simplify a lot and remove duplicate code, extends and overrides now, also set GeocoderTest to ignore as all those tests rely on internet

--- MainBuild/src/main/java/dataHandler/RetailerDataHandlerFake.java
+++ MainBuild/src/main/java/dataHandler/RetailerDataHandlerFake.java
@@ -4,52 +4,15 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
- * Created by jes143 on 18/09/17.
+ * A fake RetailerDataHandler that overrides the process line function to remove the geocoder
  */
 
-public class RetailerDataHandlerFake implements DataHandler, GeoCallback {
+public class RetailerDataHandlerFake extends RetailerDataHandler {
 
-    private SQLiteDB db;
-
-    private String[] fields =
-            {"RETAILER_NAME      VARCHAR(50) NOT NULL",
-                    "ADDRESS            VARCHAR(50) NOT NULL",
-                    "LAT                NUMERIC(9,6) NOT NULL",
-                    "LONG               NUMERIC(9,6) NOT NULL",
-                    "CITY               VARCHAR(20)",
-                    "STATE              VARCHAR(2)",
-                    "ZIP                VARCHAR(8)",
-                    "Main_Type          VARCHAR(50)",
-                    "Secondary_Type     VARCHAR(50)",
-                    "list_name          VARCHAR(25)"};
-
-    private String primaryKey = "RETAILER_NAME, ADDRESS";
-    private String tableName = "retailer";
-
-    private PreparedStatement addData;
-    private String addDataStatement = "insert or fail into retailer values(?,?,?,?,?,?,?,?,?,?)";
-    private int[] fieldCounts = {9, 18};
-
-    /**
-     * Initializes an object, linked to the given database. Can process CSVs and add single entries
-     *
-     * @param db
-     */
     public RetailerDataHandlerFake(SQLiteDB db) {
-        this.db = db;
-        db.addTable(tableName, fields, primaryKey);
-        addData = db.getPreparedStatement(addDataStatement);
-
+        super(db);
     }
-
-    /**
-     * Processes a CSV line and adds to the database if valid.
-     * Has the ability to handle the new and old format of CSV from data.gov
-     * To add support for a new format, just add some extra checks, and if there is a different number of fields, add that number to the fieldCounts list
-     *
-     * @param record A string array of object corresponding to the CSV
-     * @return A bool stating the success state of the process.
-     */
+    @Override
     public void processLine(String[] record, Callback callback) {
         System.out.println("here");
         if (record.length == 18 && !record[10].equals("")) {
@@ -68,69 +31,5 @@ public class RetailerDataHandlerFake implements DataHandler, GeoCallback {
         } else {
             callback.result(false);
         }
-    }
-
-    /**
-     * Takes a full list of parameters for an element in the table and adds that to the database using a PreparedStatement
-     *
-     * @param RETAILER_NAME
-     * @param ADDRESS
-     * @param LAT
-     * @param LONG
-     * @param CITY
-     * @param STATE
-     * @param ZIP
-     * @param MAIN_TYPE
-     * @param SECONDARY_TYPE
-     * @return A value representing the success of the addition. Fails on such things as PrimaryKey collisions.
-     */
-    public Boolean addSingleEntry(
-            String RETAILER_NAME, String ADDRESS, double LAT, double LONG, String CITY,
-            String STATE, String ZIP, String MAIN_TYPE, String SECONDARY_TYPE) {
-        try {
-            addData.setObject(1, RETAILER_NAME);
-            addData.setObject(2, ADDRESS);
-            addData.setObject(3, LAT);
-            addData.setObject(4, LONG);
-            addData.setObject(5, CITY);
-            addData.setObject(6, STATE);
-            addData.setObject(7, ZIP);
-            addData.setObject(8, MAIN_TYPE);
-            addData.setObject(9, SECONDARY_TYPE);
-            addData.setObject(10, "list");
-            addData.executeUpdate();
-            return true;
-        } catch (SQLException e) {
-            addData = db.getPreparedStatement(addDataStatement);
-            System.out.println(e.getMessage());
-            return false;
-        }
-    }
-
-    @Override
-    public void result(String[] record, double[] latlon, Callback callback) {
-        System.out.println("5");
-        System.out.println(latlon[0]);
-        System.out.println(latlon[1]);
-        callback.result(addSingleEntry(record[0], record[1], latlon[0], latlon[1], record[3], record[4], record[5], record[7], record[8]));
-    }
-
-    @Override
-    public boolean canProcess(int columnCount) {
-        for (int count: fieldCounts) {
-            if (columnCount == count) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public String getFieldCounts() {
-        String out = "";
-        for (int count: fieldCounts) {
-            out += Integer.toString(count) + " or ";
-        }
-        return out.substring(0, out.length() - 4);
     }
 }
